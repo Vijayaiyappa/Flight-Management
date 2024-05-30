d42248091f18563ab257a99a0a0a9ef0ee88abfa
Vijayaiyappa <vijayaiyapparock@gmail.com>
2024-05-30 10:23:05 +0100

    commit

---
+++ Vijay-Aiyappa-Celery/proxy_list/task_scheduler/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import requests
+
+class Proxy(models.Model):
+    ip = models.CharField(max_length=50)
+    port = models.IntegerField()
+    protocol = models.CharField(max_length=10)
+    country = models.CharField(max_length=100)
+    uptime = models.CharField(max_length=100)
